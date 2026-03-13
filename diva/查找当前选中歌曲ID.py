00553cc73005bae5afa085ccc3e18791ae271c83
hiki8man <63759994+hiki8man@users.noreply.github.com>
2026-03-14 03:31:49 +0800

    Add files via upload

--- diva/查找当前选中歌曲ID.py
+++ diva/查找当前选中歌曲ID.py
@@ -14,14 +14,15 @@ def get_select_pvid() -> int:
     返回 0 代表没有选中歌曲
     否则返回pvid
     '''
-    currect_pvid_base_address = game_pymem.read_int(game_address + int("0xCC5EF18" , 16))
+    jump_address = game_pymem.read_uint(game_address + int("0x6efe8c" , 16))
+    currect_pvid_base_address = game_pymem.read_uint(game_address + int("0xCC5EF18" , 16))
     if currect_pvid_base_address == 0 or not isinstance(currect_pvid_base_address, int):
         # 尝试获取打歌界面pvid
         # 打歌界面pvid空值为0xFFFFFFFF
         currect_pvid = game_pymem.read_uint(game_address + int("0x16E2BB0" , 16))
     else:
         # 选歌界面pvid空值为0xFFFFFFFE
-        pvid_address = currect_pvid_base_address + int("0x000258DC", 16)
+        pvid_address = currect_pvid_base_address + jump_address - int("0x9c8", 16)
         currect_pvid = game_pymem.read_uint(pvid_address)
 
     if currect_pvid in (int("0xFFFFFFFF", 16),int("0xFFFFFFFE", 16)) or not isinstance(currect_pvid, int):
