17d223b83956f8cf0aa2226fdb99d2a334a3474c
hiki8man <63759994+hiki8man@users.noreply.github.com>
2026-05-22 17:23:42 +0800

    优化模板

--- OSU/个人主页转image/main.py
+++ OSU/个人主页转image/main.py
@@ -4,9 +4,11 @@ import httpx
 import json
 import re
 import html
+import imgkit
+from pathlib import Path
 
 GET_USER_JSON = re.compile(r'data-initial-data="(.*)"\s+data-react="profile-page"')
-
+KIT = Path().joinpath("wkhtmltox","bin","wkhtmltoimage.exe")
 def get_user_json(osu_id:int) -> dict:
     with httpx.Client(base_url="https://osu.ppy.sh",verify=False) as client:
         response = client.get(f"users/{osu_id}")
@@ -19,25 +21,25 @@ def get_user_json(osu_id:int) -> dict:
         else:
             return {}
 
+
 def get_profile_image(user_id:int) -> bytes:
 
     data = get_user_json(user_id)
-    with open("template.html", "r", encoding="utf-8") as f:
+    with open("template2.html", "r", encoding="utf-8") as f:
         html = f.read()
 
-    with open("template.css", "r", encoding="utf-8") as f:
-        css = f.read()
-
     html = html.replace(r"{{user_page_html}}", data["user"]["page"]["html"])
-    css = css.replace(r"{{user_profile_hue}}", str(data["user"]["profile_hue"]))
+    html = html.replace(r"{{user_profile_hue}}", str(data["user"]["profile_hue"]))
 
     book = plutoprint.Book(size=plutoprint.PageSize(1000,-1),media=plutoprint.MEDIA_TYPE_SCREEN)
-    book.load_html(html, user_style=css)
+    book.load_html(html)
 
     data = io.BytesIO()
     book.write_to_png_stream(data)
     data.seek(0)
+    
     return  data.getvalue()
+
 
 if __name__ == "__main__":
     data = get_profile_image(15846580)
