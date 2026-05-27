e5397a97ee8cc1bb11c8b752f2084b055c44cfd4
hiki8man <63759994+hiki8man@users.noreply.github.com>
2026-05-27 12:01:01 +0800

    修正正则循环卡死的问题

--- OSU/个人主页转image/main.py
+++ OSU/个人主页转image/main.py
@@ -10,8 +10,9 @@ import time
 
 GET_USER_JSON: re.Pattern = re.compile(r'data-initial-data="(.*)"\s+data-react="profile-page"')
 IMAGEMAP_LINK: str = r'<a\s+class="imagemap__link"[^>]*?></a>'
+FIX_TSPAN: re.Pattern = re.compile(r"<tspan([^>]*)>(<tspan[^>]*>.*?</tspan>.*?[</tspan>]*)</tspan>", re.DOTALL)
 
-WEBP_MAX: int = 12000 # 最大是16383，但QQ有大小限制10mb，保险起见削弱一下
+WEBP_MAX: int = 16000 # 最大是16383，但QQ有大小限制10mb，保险起见削弱一下
 MAX_HEIGHT: int = 30000 # QQ有最大像素限制，暂时限定死3w
 QQ_PNG_SIZE_MAX: int = 1024 * 1024 # 10mb
 
@@ -49,6 +50,32 @@ class DataFeather(plutoprint.ResourceFetcher):
                 icon_data.save(data, "png")
                 data.seek(0)
                 return {"content":data.getvalue(), "mime_type":"image/png"}
+
+            case "image/svg+xml":
+                # plutobook不支持内嵌tspan，需要删除无用部分
+                def fix_tspan(match):
+                    outer = match.group(1) 
+                    inner = match.group(2)
+                    if inner.count("<tspan") >= 2 and re.search(r">[^<]+<", inner):
+                        inner = re.sub(r"</?tspan[^>]*>", "", inner)
+                        return f"<tspan{outer}>{inner}</tspan>"
+                    return match.group(0)
+                
+                content:str = response.content.decode()
+
+                content_pos:int = 0
+                while content_pos < len(content):
+                    result = FIX_TSPAN.search(content, pos=content_pos)
+                    if result:
+                        content = FIX_TSPAN.sub(fix_tspan, content)
+                        content_pos = result.start() + 1 # 修改后字符结束位置发生变化，因此使用start定位
+                    else:
+                        break
+
+                with open("temp.svg", "w", encoding="utf-8") as f:
+                    f.write(content)
+
+                return {"content":content.encode(), "mime_type":"image/svg+xml"}
             case _:
                 return {"content":response.content, "mime_type":response.headers["content-type"]}
 
@@ -73,7 +100,8 @@ def get_profile_image(user_id:int) -> None:
     html = html.replace(r"{{user_avatar}}", f"https://a.ppy.sh/{data["user"]["id"]}")
     html = html.replace(r"{{user_name}}", data["user"]["username"])
     html = html.replace(r"{{user_page_html}}", data["user"]["page"]["html"])
-    css  =  css.replace(r"{{user_profile_hue}}", str(data["user"]["profile_hue"]))
+    hue = data["user"]["profile_hue"] if data["user"]["profile_hue"]!=None else 333
+    css  =  css.replace(r"{{user_profile_hue}}", str(hue))
 
     book = plutoprint.Book(
         size=plutoprint.PageSize(1000, -1), 
@@ -88,27 +116,28 @@ def get_profile_image(user_id:int) -> None:
         html_debug = html + "<style>" + css + "</style>"
         f.write(html_debug)
 
-    data = io.BytesIO()
-    book.load_html(html, user_style=css)
+    with io.BytesIO() as data:
+        book.load_html(html, user_style=css)
 
-    doc_height = book.get_document_height()
-    if doc_height > MAX_HEIGHT:
-        image_height = MAX_HEIGHT
-    else:
-        image_height = doc_height
+        doc_height = book.get_document_height()
+        if doc_height > MAX_HEIGHT:
+            image_height = MAX_HEIGHT
+        else:
+            image_height = doc_height
 
-    book.write_to_png_stream(data, height=int(image_height))
+        book.write_to_png_stream(data, height=int(image_height))
 
-    data.seek(0)
-    image = Image.open(data)
+        data.seek(0)
+        image = Image.open(data)
+        image = image.convert("RGB")
 
     if image_height <= WEBP_MAX:
-        image.save(f"{user_id}.webp")
+        image.save(f"{user_id}.webp", quality=90)
     else:
-        image.save(f"{user_id}.jpeg")
+        image.save(f"{user_id}.jpeg", optimize=True, quality=90)
 
 if __name__ == "__main__":
     start_time = time.time()
-    get_profile_image(18230719)
+    get_profile_image(33726949)
     end_time = time.time()
     print(f"Execution time: {end_time - start_time} seconds")
