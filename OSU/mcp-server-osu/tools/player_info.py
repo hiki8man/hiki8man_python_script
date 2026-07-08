4530ed8c30d24f6b094713a6d2994b5a5c744a77
hiki8man <63759994+hiki8man@users.noreply.github.com>
2026-07-08 18:44:54 +0800

    Add files via upload

--- OSU/mcp-server-osu/tools/player_info.py
+++ OSU/mcp-server-osu/tools/player_info.py
@@ -1,18 +1,41 @@
 from fastmcp.tools import tool
+from fastmcp.prompts import prompt
 from enum import StrEnum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class MapType(StrEnum):
-    BEATMAP    = "b"
-    BEATMAPSET = "s"
+    BEATMAP = "beatmap"
+    BEATMAPSET = "beatmapset"
+
+class BeatmapSetInfo(BaseModel):
+    title: str  = Field(..., description="歌名（英文）")
+    title_unicode:str = Field(..., description="歌名")
+    artist: str = Field(..., description="艺术家（英文）")
+    artist_unicode:str = Field(..., description="艺术家")
+    bpm: float  = Field(..., description="BPM")
+    creator: str = Field(..., description="谱师")
+    source: str = Field(..., description="来源")
+
+class BeatmapInfo(BeatmapSetInfo):
+    beatmap_id: str = Field(..., description="单谱面ID")
 
 @tool
-def get_beatmap_info(map_type: MapType, map_id: int) -> dict:
+def get_beatmap_info(map_id: int) -> BeatmapInfo:
+    """
+    Description:   
+        - 获取osu!谱面集详细信息：标题、艺术家、BPM、难度参数等
+
+    Args:
+        - map_type (MapType): 查询类型，可选 "beatmap"（单谱面）或 "beatmapset"（谱面集）   
+        - map_id (int): 谱面ID
+    """
+
+@tool
+def get_beatsetmap_info(map_id: int) -> BeatmapSetInfo:
     """
     Description:   
         - 获取osu!谱面详细信息：标题、艺术家、BPM、难度参数等
 
     Args:
-        - map_type (MapType): 查询类型，可选 "beatmap"（单谱面）或 "beatmapset"（谱面集）   
         - map_id (int): 谱面ID
     """
